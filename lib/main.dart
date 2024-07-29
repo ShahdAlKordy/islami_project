a555d87f17faf7b8201ea11491ffe1dfcb4428b2
shahd <shahdmkorddy6@gmail.com>
2024-07-29 14:58:47 +0300

    Inti project. Added empty home screen.

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:islami/ui/chapterDetails/chapter_details.dart';
+import 'package:islami/ui/hadethDetails/hadeth_details.dart';
+import 'package:islami/ui/home/home_screen.dart';
+import 'package:islami/ui/splash/splash_screen.dart';
+import 'package:islami/ui/theme/MyTheme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: SplashScreen.routeName,
+      routes: {
+        SplashScreen.routeName: (_) => SplashScreen(),
+        HomeScreen.routeName: (_) => HomeScreen(),
+        ChapterDetails.routeName: (_) => ChapterDetails(),
+        HadethDetailsScreen.routeName: (_) => HadethDetailsScreen(),
+      },
+      theme: MyThemeData.lightTheme,
+    );
+  }
+}
